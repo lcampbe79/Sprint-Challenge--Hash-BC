2617db6f2a6d0e31816980e1933c6d5ff3db97c0
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-04-10 10:36:30 -0700

    hashtable ex1 and updated nblockchain.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,6 +12,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for i in range(length):
+
+        #retrieve takes the hash table and key for every index, finds weight_list_key corresponding to key
+        #limit - weight[i] = another weight[i] or a key! 
+        #weight_list_key will return none if key is not found, otherwise retrieves the weight_list_key stored with the key 
+        weight_list_key = hash_table_retrieve(ht, (limit - weights[i]))
+        #populate the hash table, storing the weight_list_key w/ the given key 
+        hash_table_insert(ht, weights[i], i)
+
+        #i and weight_list_key are indices of two weights that equal the limit
+        if weight_list_key is not None:
+            # The higher value index (weight_list_key) should be placed in the zeroth index and the smaller index should be placed in the first index. 
+            if i > weight_list_key:
+                return [i, weight_list_key]
+            else:
+                return [weight_list_key, i]
 
     return None
 
